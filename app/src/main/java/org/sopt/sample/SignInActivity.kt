c0d6557308df6c427c125bc2694569edc5978515
s9hn <tnghqn@naver.com>
2022-10-14 03:05:59 +0900

    [Feat] 기초세팅
    1. 폰트추가
    2. 이미지추가
    3. 스타일가이드 추가
    4. 컬러 추가

---
+++ app/src/main/java/org/sopt/sample/SignInActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
