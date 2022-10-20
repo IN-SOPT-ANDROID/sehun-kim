41ce35a7270427658eb0c77c7a66b42f40afafb5
s9hn <tnghqn@naver.com>
2022-10-20 22:41:35 +0900

    [Chore] 깃깃

--- app/src/main/java/org/sopt/sample/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/presentation/home/HomeFragment.kt
@@ -12,11 +12,7 @@ import org.sopt.sample.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
