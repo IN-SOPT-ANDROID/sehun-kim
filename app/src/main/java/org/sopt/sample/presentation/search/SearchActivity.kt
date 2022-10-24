ae901d09dee00a32a1a191cb6fd3d07f24176fac
s9hn <tnghqn@naver.com>
2022-10-24 22:17:14 +0900

    [Feat] 검색 Activity

---
+++ app/src/main/java/org/sopt/sample/presentation/search/SearchActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.sample.presentation.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
