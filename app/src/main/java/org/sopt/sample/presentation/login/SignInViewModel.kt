fb816d0c44e26b39c2e7b04e5a0b147cc4eeb8bd
s9hn <tnghqn@naver.com>
2022-11-10 17:24:43 +0900

    [Feat(SignInViewModel)] id,password 변수 추가

--- app/src/main/java/org/sopt/sample/presentation/login/SignInViewModel.kt
+++ app/src/main/java/org/sopt/sample/presentation/login/SignInViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SignInViewModel() : ViewModel() {
+    val id = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
     private val _isChecked = MutableLiveData<Boolean>()
     val isChecked: LiveData<Boolean> get() = _isChecked
 
