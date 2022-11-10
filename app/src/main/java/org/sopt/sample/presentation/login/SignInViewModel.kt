d61c06febe5eed6301d7365477657d29b6ba1a9a
s9hn <tnghqn@naver.com>
2022-11-10 17:11:39 +0900

    [Feat] 안되겠음

--- app/src/main/java/org/sopt/sample/presentation/login/SignInViewModel.kt
+++ app/src/main/java/org/sopt/sample/presentation/login/SignInViewModel.kt
@@ -1,31 +1,13 @@
 package org.sopt.sample.presentation.login
 
-import android.annotation.SuppressLint
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import org.sopt.sample.data.local.LoginInfo
-import org.sopt.sample.util.LoginSharedPreferences
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
-class SignInViewModel(application: Application) : AndroidViewModel(application) {
-    @SuppressLint("StaticFieldLeak")
-    private val context = getApplication<Application>().applicationContext
-    private val loginSharedPreferences by lazy { LoginSharedPreferences(context) }
+class SignInViewModel() : ViewModel() {
+    private val _isChecked = MutableLiveData<Boolean>()
+    val isChecked: LiveData<Boolean> get() = _isChecked
 
-    fun setSharedPreferences(id: String, pw: String, value: Boolean) {
-        // 유저데이터 및 자동로그인 체크 여부 전달
-        with(loginSharedPreferences) {
-            saveLoginInfo(
-                LoginInfo(
-                    id = id,
-                    pw = pw
-                )
-            )
-            setAutoLogin(value)
-        }
-    }
-
-    fun checkAutoLogin(): Boolean {
-        // SignInActivity True/False 값 넘겨줌
-        return loginSharedPreferences.getAutoLogin(context)
-    }
+    fun log() = Log.d("dd", isChecked.toString())
 }
